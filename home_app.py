72e13909f620403f4ba4aa8254dfb129cb67fbdb
Ian <iansous@gmail.com>
2024-04-12 21:07:35 -0300

    finalizing requeriments

--- home_app.py
+++ home_app.py
@@ -1,13 +1,10 @@
 ###################### IMPORTS ################################
-import warnings
 import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
 import seaborn as sns
 import statsmodels.api as sm
 import streamlit as st
-
-warnings.filterwarnings('ignore')
 ###################### IMPORTS ################################
 
 ###################### HELPER FUNCTIONS ################################
